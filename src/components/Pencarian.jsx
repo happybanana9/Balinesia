ac35023544faf2814effcdba7c74134542f68081
happybanana9 <firemars0923@gmail.com>
2022-06-06 00:54:46 +0800

    Menambah dropdown di page pencarian

--- src/components/Pencarian.jsx
+++ src/components/Pencarian.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Dropdown from './Dropdown'
 
 const Pencarian = () => {
+  const options = ['New', 'Edit', 'Update', 'Delete']
     return (
-    <section>
         <section id="beranda">
           <div className="w-full h-screen relative bg-theme3">
             <div className="absolute top-20 w-full h-full flex flex-col text-center text-theme1">
-                <h1 className="font-black text-3xl">Pencarian</h1>
+                <h1 className="font-black text-3xl">Pencarian Destinasi Wisata di Provinsi Bali</h1>
+                <Dropdown options={options}/>
             </div>     
           </div>
         </section>
-      </section>
     )
   }
   
