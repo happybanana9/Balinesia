7ee488cdaa2bde740df0da4003b2b95208a81637
happybanana9 <firemars0923@gmail.com>
2022-06-05 11:12:01 +0800

    Menambah page pencarian

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Recommendation from './pages/Recommendation';
 import Browsing from './pages/Browsing';
+import Searching from './pages/Searching';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
         <NavBar />
         <Routes>
           <Route path='/' element={<Homepage />} />
-          <Route path='/cari' element={<Homepage />} />
+          <Route path='/cari' element={<Searching />} />
           <Route path='/jelajah' element={<Browsing />} />
           <Route path='/rekomendasi' element={<Recommendation />} />
         </Routes>
---
+++ src/pages/Searching.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Footer } from '../components/Footer'
+import Pencarian from '../components/Pencarian'
+
+function Searching() {
+    return (
+      <><Footer /><Pencarian /></>
+    )
+  }
+  
+  export default Searching
