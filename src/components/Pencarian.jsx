81e87cf5fbd297d46d0900cbba33b45b5ea6577a
happybanana9 <firemars0923@gmail.com>
2022-06-21 20:48:23 +0700

    Move Dropdown to Pencarian

--- src/components/Pencarian.jsx
+++ src/components/Pencarian.jsx
@@ -1,30 +1,266 @@
 /* eslint-disable prettier/prettier */
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import Cards from "./Cards";
-import Dropdown from "./Dropdown";
+import { Listbox, Transition } from "@headlessui/react";
+import { CheckIcon, SelectorIcon } from "@heroicons/react/solid";
+
+const Location = [
+  { name: "Lokasi" },
+  { name: "Karangasem" },
+  { name: "Bangli" },
+  { name: "Gianyar" },
+  { name: "Buleleng" },
+  { name: "Klungkung" },
+  { name: "Badung" },
+  { name: "Tabananan" },
+  { name: "Jembrana" },
+  { name: "Denpasar" },
+];
+
+const Rating = [
+  { rate: "Rating" },
+  { rate: "⭐4-⭐5" },
+  { rate: "⭐3-⭐4" },
+  { rate: "⭐2-⭐3" },
+  { rate: "⭐1-⭐2" },
+];
+const Harga = [
+  { price: "Harga" },
+  { price: "Diatas 100.000" },
+  { price: "50.000 - 100.000" },
+  { price: "25.000 - 50.000" },
+  { price: "Gratis" },
+];
 
 const Pencarian = () => {
+  const [selected, setSelected] = useState(Location[0]);
+  const [selectedrate, setSelectedRate] = useState(Rating[0]);
+  const [selectedprice, setSelectedPrice] = useState(Harga[0]);
   const [wisata, setWisata] = useState([]);
-  const getWisataRekomendasi = async () => {
-    const response = await axios.get('http://localhost:5000/api/destinations');
+  const getWisata = async () => {
+    const response = await axios.get("http://localhost:5000/api/destinations");
     const resData = response.data.data;
-    setWisata(resData);
-  }
-
-  useEffect(()=>{
-    getWisataRekomendasi()
-  },[])
+    const resFilter = resData.filter((res) => res.lokasi == selected.name);
+    setWisata(resFilter);
+    console.log(selected)
+  };
+  useEffect(() => {
+    getWisata();
+  }, []);
   return (
     <section id="Pencarian">
-      <div className="w-full h-full relative bg-theme3">
+      <div className="w-full h-screen relative bg-theme3">
         <div className="relative px-8 py-32 md:max-w-none text-center text-theme1">
           <h1 className="font-black text-3xl">Pencarian Destinasi Wisata di</h1>
           <h1 className="font-black text-3xl">Provinsi Bali</h1>
-          <Dropdown />
+          <div className="flex flex-wrap items-center justify-center mt-5">
+            <div className="inline-flex flex-col md:flex-row text-left mb-4 md:mb-2">
+              {/* Dropdown Location Start */}
+              <Listbox value={selected} onChange={setSelected}>
+                <div className="relative mt-1 mx-5">
+                  <Listbox.Button className="relative w-full h-auto cursor-default rounded-lg bg-white py-2 p-20 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate text-theme3">
+                      {selected.name}
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {Location.map((location, locationId) => (
+                        <Listbox.Option
+                          key={locationId}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? "bg-amber-100 text-amber-900"
+                                : "text-gray-900"
+                            }`
+                          }
+                          value={location}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? "font-medium" : "font-normal"
+                                }`}
+                              >
+                                {location.name}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+              {/* Dropdown Location Start */}
+              <Listbox value={selectedrate} onChange={setSelectedRate}>
+                <div className="relative mt-1 mx-5">
+                  <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 p-20 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate text-theme3">
+                      {selectedrate.rate}
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {Rating.map((rates, ratesId) => (
+                        <Listbox.Option
+                          key={ratesId}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? "bg-amber-100 text-amber-900"
+                                : "text-gray-900"
+                            }`
+                          }
+                          value={rates}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? "font-medium" : "font-normal"
+                                }`}
+                              >
+                                {rates.rate}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+              {/* Dropdown Location Start */}
+              <Listbox value={selectedprice} onChange={setSelectedPrice}>
+                <div className="relative mt-1 mx-5">
+                  <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 p-20 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+                    <span className="block truncate text-theme3">
+                      {selectedprice.price}
+                    </span>
+                    <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+                      <SelectorIcon
+                        className="h-5 w-5 text-gray-400"
+                        aria-hidden="true"
+                      />
+                    </span>
+                  </Listbox.Button>
+                  <Transition
+                    as={Fragment}
+                    leave="transition ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                      {Harga.map((prices, priceId) => (
+                        <Listbox.Option
+                          key={priceId}
+                          className={({ active }) =>
+                            `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                              active
+                                ? "bg-amber-100 text-amber-900"
+                                : "text-gray-900"
+                            }`
+                          }
+                          value={prices}
+                        >
+                          {({ selected }) => (
+                            <>
+                              <span
+                                className={`block truncate ${
+                                  selected ? "font-medium" : "font-normal"
+                                }`}
+                              >
+                                {prices.price}
+                              </span>
+                              {selected ? (
+                                <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                                  <CheckIcon
+                                    className="h-5 w-5"
+                                    aria-hidden="true"
+                                  />
+                                </span>
+                              ) : null}
+                            </>
+                          )}
+                        </Listbox.Option>
+                      ))}
+                    </Listbox.Options>
+                  </Transition>
+                </div>
+              </Listbox>
+            </div>
+            <button onClick={getWisata} className="inline-flex items-center px-5 py-2 mx-4 bg-indigo-500 hover:bg-indigo-600 text-white text-sm font-medium rounded-3xl max-h-10 max-w-sm">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5 mr-2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <a>Cari Destinasi</a>
+            </button>
+          </div>
           <div className="relative z-0 mt-12 gap-8 flex-wrap flex justify-center items-center">
-          {wisata.map((value,index)=>{
-         return <section style={{ backgroundImage: `url('${value.picture_url}')` }} className="card-container"key={index}><Cards item={value}/></section>})}
+            {wisata.map((value, index) => {
+              return (
+                <section
+                  style={{ backgroundImage: `url('${value.picture_url}')` }}
+                  className="card-container"
+                  key={index}
+                >
+                  <Cards item={value} />
+                </section>
+              );
+            })}
           </div>
         </div>
       </div>
